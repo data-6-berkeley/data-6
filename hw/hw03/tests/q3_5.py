5b029513de46a743de2385a3cb9f7427bb9903fc
castroian <42158157+castroian@users.noreply.github.com>
2020-07-21 11:51:57 -0700

    Update q3_5.py

--- hw/hw03/tests/q3_5.py
+++ hw/hw03/tests/q3_5.py
@@ -6,15 +6,6 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> import numpy as np
-          >>> type(votes_for_clinton) == numpy.int64
-          True
-          """,
-          'hidden': False,
-          'locked': False
-        },
-        {
-          'code': r"""
           >>> votes_for_clinton == 65853581
           True
           """,
