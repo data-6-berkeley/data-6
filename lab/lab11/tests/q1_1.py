3763ff3f260ae5dcc0839585a06986e9f20f6ef9
castroian <42158157+castroian@users.noreply.github.com>
2020-07-26 15:19:32 -0700

    Add week 4 content

---
+++ lab/lab11/tests/q1_1.py
@@ -0,0 +1,23 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> total_pay_type == str
+          True
+
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
