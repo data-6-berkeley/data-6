d3db1c1aa16f4cb8f2dea06b913a2fd5fd090c2a
castroian <42158157+castroian@users.noreply.github.com>
2020-08-03 16:29:55 -0700

    Update q1_6.py

--- hw/hw04/tests/q1_6.py
+++ hw/hw04/tests/q1_6.py
@@ -15,7 +15,7 @@ test = {
         {
           'code': r"""
           >>> import numpy as np
-          >>> np.round(sum(ces_with_distances.column("distance_from_oak"))) == 23412011.0
+          >>> np.round(sum(ces_with_distances.column("distance_from_oak"))) == 23412008.0
           True
           """,
           'hidden': False,
